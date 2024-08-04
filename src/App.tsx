4c88192828e3bea3ddc71acf0e07559b57a94182
Shivansh Gupta <shivanshgupta@Shivanshs-MacBook-Air.local>
2024-08-04 17:41:40 +0530

    Frontend Basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,39 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import {Input} from "@nextui-org/input";
-
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Input } from "@nextui-org/input";
 
 const App = () => {
-
-  const placements = [
-    "outside-left",
-  ] as const;
+  const placements = ["outside-left"] as const;
 
   return (
     <div className="main-app">
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'flex-end',
-        padding: 12,
-      }}
-    >
-      <ConnectButton />
-    </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          padding: 12,
+        }}
+      >
+        <ConnectButton />
+      </div>
 
-      <div className='heading'>
+      <div className="heading">
         <h1>Create ZKPs of Attestations</h1>
       </div>
 
-    <div className='input'>
+      <div className="zkp-container">
+        <div className="input-container">
+          <div className="input-uid-text">
+            <label>Enter UID:</label>
+          </div>
 
-    <div className='input-uid'>
-    <label>Enter UID:</label>
-    <input type="text" id="fname" name="fname" placeholder='UID'/>
+          <input type="text" id="fname" name="fname" placeholder="UID" />
+        </div>
+
+        <div className="submit-uid">
+          <button>Generate ZK Proof</button>
+        </div>
+      </div>
     </div>
-
-    <div className='submit-uid'>
-    <button>
-      Create Proof
-    </button>
-    </div>
-    </div> 
-
-
-
-
-    </div>
- 
-
-    
   );
 };
 
