da0c9e30edeb7628bfe1375ad63c8204490c6f20
Shivannsh <bb1221392@iitd.ac.in>
2024-08-11 20:17:54 +0530

    All console logs added

--- src/App.jsx
+++ src/App.jsx
@@ -29,10 +29,10 @@ const App = () => {
   }, [isConnected]);
 
   const handleProof = async (result) => {
-    console.log(
-      "Proof received from IDKit, sending to backend:\n",
-      JSON.stringify(result)
-    );
+    // console.log(
+    //   "Proof received from IDKit, sending to backend:\n",
+    //   JSON.stringify(result)
+    // );
 
     try {
       const response = await fetch("http://localhost:3001/verify-proof", {
@@ -51,10 +51,10 @@ const App = () => {
       const data = await response.json();
 
       if (data.success) {
-        console.log(
-          "Successful response from backend:\n",
-          JSON.stringify(data)
-        );
+        // console.log(
+        //   "Successful response from backend:\n",
+        //   JSON.stringify(data)
+        // );
         onSuccess(); // Call onSuccess without arguments
       } else {
         throw new Error(
@@ -73,14 +73,14 @@ const App = () => {
       const [type, name] = part.split(" ").map((p) => p.trim());
       return { type, name };
     });
-    console.log("abi", abiTypes);
+    // console.log("abi", abiTypes);
     return abiTypes;
   }
 
   function decodeData(abiTypes, raw_data) {
     const coder = new AbiCoder();
     const bytes = getBytes(raw_data);
-    console.log("bytes is", bytes);
+    // console.log("bytes is", bytes);
     const decodedResult = coder.decode(
       abiTypes.map((item) => item.type),
       bytes
@@ -102,7 +102,7 @@ const App = () => {
       return acc;
     }, {});
 
-    console.log("Decoded Data:", formattedResult);
+    // console.log("Decoded Data:", formattedResult);
     return formattedResult;
   }
 
@@ -121,7 +121,7 @@ const App = () => {
         return response.text();
       })
       .then((text) => {
-        console.log(text);
+        // console.log(text);
       })
       .catch((error) => {
         console.error("Error saving JSON data:", error);
@@ -134,8 +134,8 @@ const App = () => {
       return;
     }
 
-    console.log("uid", uid);
-    console.log("account", address);
+    // console.log("uid", uid);
+    // console.log("account", address);
     if (provider) {
       try {
         const { schemaUID, attest_data, recipient, attester } =
@@ -146,7 +146,7 @@ const App = () => {
             setOpenWidget(true); // Open the widget after fetching attestation data
           } else {
             const schemaRecord = await fetchSchemaRecord(provider, schemaUID);
-            console.log("schema is ", schemaRecord);
+            // console.log("schema is ", schemaRecord);
             const abiTypes = parseSchema(schemaRecord);
             const decodedData = decodeData(abiTypes, attest_data);
             saveJSON(decodedData);
@@ -166,12 +166,12 @@ const App = () => {
             }
 
             const result = await response.json();
-            console.log("ZKP result:", result);
+            // console.log("ZKP result:", result);
 
             // Extract digest and fields
             const { digest, fields } = result;
-            console.log("Digest:", digest);
-            console.log("Fields:", fields);
+            // console.log("Digest:", digest);
+            // console.log("Fields:", fields);
 
             // Set the message and stop the loader
             setZkpMessage(`
@@ -200,7 +200,7 @@ const App = () => {
   };
 
   const onSuccess = () => {
-    console.log("Proof verified successfully");
+    // console.log("Proof verified successfully");
     setIsVerified(true); // Fixed this to correctly set the state
     fetchAttestationData(uid);
     setOpenWidget(false); // Close the widget after successful verification
